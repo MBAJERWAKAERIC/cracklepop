e62e305e834cc03f937c63936093af7dec37bb13
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-03-22 20:05:48 +0300

    variables

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log(" I love Yvette!");
+console.log(" with my heart!");
+
+
+let firstName = "Bro" //string
+let age;
+age = 21; //number
+let student = true; //booleans
+
+console.log("Hello", firstName);
+console.log("you are ", age, "years old");
+console.log("enroled:", student);
+
+document.getElementById("p1").innerHTML = "Hello" + firstName;
+document.getElementById("p2").innerHTML = "you are" + age + " years old";
+document.getElementById("p3").innerHTML = "enroled: " + student;
