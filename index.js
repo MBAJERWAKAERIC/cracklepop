8e683331f20c8bdc450ad728cde3b2949396d76a
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-03-24 11:40:46 +0300

    user input

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log(" I love Yvette!");
+/*c onsole.log(" I love Yvette!");
 console.log(" with my heart!");
 
 
@@ -14,3 +14,17 @@ console.log("enroled:", student);
 document.getElementById("p1").innerHTML = "Hello" + firstName;
 document.getElementById("p2").innerHTML = "you are" + age + " years old";
 document.getElementById("p3").innerHTML = "enroled: " + student;
+
+let students = 20;
+students = students - 1;
+console.log(students);
+let username = window.prompt("what is your username?");
+console.log(username);*/
+let username
+
+document.getElementById("myButton").onclick = function() {
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("myLabel").innerHTML = username;
+
+}
